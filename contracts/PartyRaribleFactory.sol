9e0aa746006f5b82f54efca71dccbf3a58d78d8d
Anton Davydov <fetsorn@Antons-MacBook-Air.local>
2021-08-31 05:51:06 +0300

    docs: add factory address

---
+++ contracts/PartyRaribleFactory.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.5;
+
+import {InitializedProxy} from "./InitializedProxy.sol";
+import {PartyRarible, IExchangeV2} from "./PartyRarible.sol";
+
+contract PartyRaribleFactory {
+    //======== Events ========
+
+    event PartyRaribleDeployed(
+        address proxy,
+        address creator,
+        address _exchange,
+        address _nftOwner,
+        address _nftContract,
+        uint256 _tokenId,
+        IExchangeV2.AssetType _ethAssetType,
+        IExchangeV2.AssetType _nftAssetType,
+        string _name,
+        string _symbol
+    );
+
+    //======== Immutable storage =========
+
+    address public immutable logic;
+    address public immutable partyDAOMultisig;
+    address public immutable tokenVaultFactory;
+    address public immutable weth;
+
+    //======== Mutable storage =========
+
+    // PartyBid proxy => block number deployed at
+    mapping(address => uint256) public deployedAt;
+
+    //======== Constructor =========
+
+    constructor(
+        address _partyDAOMultisig,
+        address _tokenVaultFactory,
+        address _weth,
+        address _logicExchange,
+        address _logicNftOwner,
+        address _logicNftContract,
+        uint256 _logicTokenId,
+        IExchangeV2.AssetType memory _logicEthAssetType,
+        IExchangeV2.AssetType memory _logicNftAssetType
+    ) {
+        partyDAOMultisig = _partyDAOMultisig;
+        tokenVaultFactory = _tokenVaultFactory;
+        weth = _weth;
+        // deploy logic contract
+        PartyRarible _logicContract = new PartyRarible(
+            _partyDAOMultisig,
+            _tokenVaultFactory,
+            _weth
+        );
+        // initialize logic contract
+        _logicContract.initialize(
+            _logicExchange,
+            _logicNftOwner,
+            _logicNftContract,
+            _logicTokenId,
+            _logicEthAssetType,
+            _logicNftAssetType,
+            "PartyRarible",
+            "PRBL"
+        );
+        // store logic contract address
+        logic = address(_logicContract);
+    }
+
+    //======== Deploy function =========
+
+    function startParty(
+        address _exchange,
+        address _nftOwner,
+        address _nftContract,
+        uint256 _tokenId,
+        IExchangeV2.AssetType calldata _ethAssetType,
+        IExchangeV2.AssetType calldata _nftAssetType,
+        string memory _name,
+        string memory _symbol
+    ) external returns (address partyBidProxy) {
+        bytes memory _initializationCalldata = abi.encodeWithSignature(
+            "initialize(address,address,address,uint256,IExchangeV2.AssetType,IExchangeV2.AssetType,string,string)",
+            _exchange,
+            _nftOwner,
+            _nftContract,
+            _tokenId,
+            _ethAssetType,
+            _nftAssetType,
+            _name,
+            _symbol
+        );
+
+        partyBidProxy = address(
+            new InitializedProxy(logic, _initializationCalldata)
+        );
+
+        deployedAt[partyBidProxy] = block.number;
+
+        emit PartyRaribleDeployed(
+            partyBidProxy,
+            msg.sender,
+            _exchange,
+            _nftOwner,
+            _nftContract,
+            _tokenId,
+            _ethAssetType,
+            _nftAssetType,
+            _name,
+            _symbol
+        );
+    }
+}
